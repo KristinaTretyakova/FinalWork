04e0ab1e133f2d70f4fabd8cf57cf43f1dd7ad82
Kristina <kristina.yakimova.96@gmail.com>
2022-12-24 21:31:05 +0200

    Add file with algoritm

---
+++ Algoritm/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите текст через один пробел, без запятых");
+string? text = Console.ReadLine();
+string [] shortWord = text.Split(' ');
+Console.WriteLine("Слова меньше четырех символов из тех, что Вы ввели это :");
+int maxLength = 4;
+for(int i = 0; i < shortWord.Length; i++)
+{
+    if(shortWord[i].Length < maxLength)
+    {
+        Console.Write($"{shortWord[i]} ");
+    } 
+}
